059311d74b6903b5936428af208862ac9a957f9c
Taupiik <133958849+Taupiik@users.noreply.github.com>
2023-06-13 11:27:53 +0800

    Add files via upload

---
+++ 2110010274/src/salary/tblUser.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salary;
+
+/**
+ *
+ * @author ASUS
+ */
+public class tblUser {
+    private String passId, userId, levelUser;
+    
+    public void setlevelUser(String levelUser){
+        this.levelUser = levelUser;
+    }
+    public String getlevelUser(){
+        return levelUser;
+    }
+    
+    public void setuserId(String userId){
+        this.userId = userId;
+    }
+    public String getuserId(){
+        return userId;
+    }
+    
+    public void setpassId(String passId){
+        this.passId = passId;
+    }
+    public String getpassId(){
+        return passId;
+    }
+}
